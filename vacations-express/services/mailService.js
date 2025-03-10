3de29982f67f83f55816f8f1643a5b4ab18c1f26
C410lol <gcaio7463@gmail.com>
2025-03-09 22:58:43 -0300

    Creating mail service

---
+++ vacations-express/services/mailService.js
@@ -0,0 +1,23 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'gcaio7463@gmail.com',
+        pass: 'bjur klcb hjel pikd'
+    }
+});
+
+
+
+
+export const sendAuthLinkEmail = (emailTo, token) => {
+    const mailOptions = {
+        from: 'gcaio7463@gmail.com',
+        to: emailTo,
+        subject: 'Link de acesso',
+        text: `Clique no link ou copie e cole em seu navegador para acessar o sistema: http://localhost:5173/auth?token=${token}`
+    }
+
+    return transport.sendMail(mailOptions);
+}
